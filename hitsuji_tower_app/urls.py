8cd95a4a1db852e6a65de9ddda5bf6115ccc3c94
himarupi <himarupi@outlook.jp>
2024-05-02 03:00:35 +0900

    first commit

--- hitsuji_tower_app/urls.py
+++ hitsuji_tower_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 # urls.py
 urlpatterns = [
     path("example/", views.example_view, name="example"),
     # Add more paths here
-]
+] + staticfiles_urlpatterns()
