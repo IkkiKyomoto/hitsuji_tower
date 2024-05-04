0c4f1cb460fb6506f2db36f8b5a1f986204b57e3
IkkiKyomoto <kazu06851@gmail.com>
2024-05-04 10:19:24 +0900

    fix: api完成

--- hitsuji_tower_map_api/serializers.py
+++ hitsuji_tower_map_api/serializers.py
@@ -5,35 +5,63 @@ from .models import Map, Room, RoomColumn, Column, MapRoom
 class ColumnSerializer(serializers.ModelSerializer):
     class Meta:
         model = Column
-        fields = "__all__"
+        fields = ["pos_y", "content"]
 
 
 class RoomColumnSerializer(serializers.ModelSerializer):
+    column = ColumnSerializer(many=True, read_only=True)
+
     class Meta:
-        model = RoomColumn
-        fields = ["pos_y", "column"]
+        model = Column
+        fields = ["column"]
 
     def get_columns(room_id):
         return Column.objects.filter(room=room_id)
 
 
-class MapRoomSerializer(serializers.ModelSerializer):
-    columns = RoomColumnSerializer(many=True, read_only=True)
+class RoomSerializer(serializers.ModelSerializer):
+    columns = serializers.SerializerMethodField()
 
     class Meta:
-        model = MapRoom
-        fields = ["room_height", "room_width", "pos_x", "pos_y", "columns"]
+        model = Room
+        fields = ["pos_x", "pos_y", "height", "width", "columns"]
 
-    def get_rooms(map_id):
+    def get_columns(self, obj):
+        room_columns = RoomColumn.objects.filter(room=obj)
+        columns = [room_column.column for room_column in room_columns]
+        return ColumnSerializer(columns, many=True).data
+
+
+class MapRoomSerializer(serializers.ModelSerializer):
+    room = RoomColumnSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Room
+        fields = ["room"]
+
+    def get_rooms(self, obj, map_id):
         return Room.objects.filter(map=map_id)
 
 
 class MapSerializer(serializers.ModelSerializer):
-    rooms = MapRoomSerializer(many=True, read_only=True)
+    rooms = serializers.SerializerMethodField()
 
     class Meta:
         model = Map
-        fields = ["id", "map_height", "map_width", "rooms", "jump"]
+        fields = ["map_height", "map_width", "rooms", "jump"]
 
-    def get_latest_map():
+    def get_rooms(self, obj):
+        map_rooms = MapRoom.objects.filter(map=obj)
+        rooms = [map_room.room for map_room in map_rooms]
+        return RoomSerializer(rooms, many=True).data
+
+
+class LatestMapSerializer(serializers.ModelSerializer):
+    map = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Map
+        fields = ["map"]
+
+    def get_latest_map(self):
         return Map.objects.latest("id")
--- hitsuji_tower_map_api/views.py
+++ hitsuji_tower_map_api/views.py
@@ -11,14 +11,8 @@ from rest_framework.response import Response
 class MapViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.AllowAny]
     serializer_class = MapSerializer
-    queryset = Map.objects.all()
+    initAll()
 
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        latest_map = Map.objects.latest("id")
+        return [latest_map]
