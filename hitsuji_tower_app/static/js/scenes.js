7c537c34b6e0024e221f57b6458a558cd2948da7
himarupi <himarupi@outlook.jp>
2024-05-05 13:25:44 +0900

    add: ひつじ仙人追加

--- hitsuji_tower_app/static/js/scenes.js
+++ hitsuji_tower_app/static/js/scenes.js
@@ -62,7 +62,6 @@ function gameOnLoad(){
 
     // プレイヤー
     //    プレイヤーの座標は右下
-    let playeMaprPos = [1, 13];
     game.data.objects.player = new Player(
         100,
         100,
@@ -72,11 +71,22 @@ function gameOnLoad(){
         game.config.playerSize
     );
 
+    game.data.objects.sheep = new Sheep(
+        100,
+        100,
+        game.data.objects.map.goalPos[0],
+        game.data.objects.map.goalPos[1],
+        game.config.sheepSize,
+        game.config.sheepSize*1.38
+    );
+    console.log(game.data.objects.sheep);
 }
 
 function gameUpdate(){
     // player 入力受け取り
     game.data.objects.player.move();
+    // sheep
+    game.data.objects.sheep.move();
 
     // mapとプレイヤーの初期値設定
     setMapAndPlayer();
@@ -89,4 +99,6 @@ function gameDraw(){
     game.data.objects.map.draw();
     // player
     game.data.objects.player.draw();
+    // sheep
+    game.data.objects.sheep.draw();
 }
